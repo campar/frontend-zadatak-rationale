5bb461669bf133f88a4359b5bbd5d7e83ca5d3c6
campar <kralj.david.david@gmail.com>
2023-03-30 23:28:49 +0200

    Display meetings data on the calendar sidebar

---
+++ src/app/components/calendar-sidebar/calendar-sidebar.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-sidebar',
+  templateUrl: './calendar-sidebar.component.html',
+})
+export class CalendarSidebarComponent {}
--- src/app/components/meeting-card/meeting-card.component.ts
+++ src/app/components/meeting-card/meeting-card.component.ts
@@ -3,7 +3,18 @@ import { Calendar } from '../../models/calendar.model';
 
 @Component({
   selector: 'meeting-card',
-  templateUrl: './meeting-card.component.html',
+  template: `
+    <div class="meeting">
+      <div class="meeting__wrapper">
+        <div class="meeting__name">{{ meeting.name }}</div>
+        <div class="meeting__date">{{ meeting.date | date : 'M.dd.y' }}</div>
+      </div>
+
+      <div *ngIf="meeting.status" class="meeting__status">
+        {{ meeting.status }}
+      </div>
+    </div>
+  `,
 })
 export class MeetingCardComponent {
   @Input() meeting!: Calendar;
