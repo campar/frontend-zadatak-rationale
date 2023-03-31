7aa08637c412d6462f3bb9e7ae20ed69526236f7
campar <kralj.david.david@gmail.com>
2023-03-31 07:41:57 +0200

    Create notification popover

---
+++ src/app/components/popover/popover.component.ts
@@ -0,0 +1,10 @@
+import { Component, ElementRef, HostListener, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-popover',
+  templateUrl: './popover.component.html',
+  styleUrls: ['./popover.component.scss'],
+})
+export class PopoverComponent {
+  @Input() isActive!: boolean;
+}
